c5929fa531ec02b0e053c05d3fefc7cbc8b81075
pradeepbasyal2003 <pradeepbasyal2003@gmail.com>
2024-11-19 19:23:24 +0545

    landing page almost complete

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import *
-
+import random
 # Create your views here.
 
 class Base(View):
@@ -16,4 +16,12 @@ class HomeView(Base):
         self.views['sliders'] = Slider.objects.all()
         self.views['ads'] = Ad.objects.all()
         self.views['products'] = Product.objects.all()
+
+        # here we store the products as list because we cant assign the whole object to an variable view garna lai yesto garna praxa
+        self.product = list(Product.objects.filter(labels = "new"))
+        random.shuffle(self.product)
+        self.views['recommended_1'] = self.product
+        random.shuffle(self.product)
+        self.views['recommended_2'] = self.product
+        #two recommended are used because we have two sliding section that has recomended and duitai same aauxa eeuta matra recommended vayo vane
         return render(request,'index.html',self.views)
