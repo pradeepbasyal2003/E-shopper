015460979b57d27945a2c6d6130c25835c0a53a5
pradeepbasyal2003 <pradeepbasyal2003@gmail.com>
2024-12-18 22:05:27 +0545

    rest api crud operation

--- home/api_urls.py
+++ home/api_urls.py
@@ -8,4 +8,8 @@ router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('product_filter' ,ProductListView.as_view() , name="product_filter"),
+    path('product_crud/<int:pk>', ProductDetail.as_view(), name="product_crud"),
+    path('product_add', ProductList.as_view(), name="product_add"),
+
 ]
--- home/api_views.py
+++ home/api_views.py
@@ -2,6 +2,71 @@
 from rest_framework import viewsets
 from .models import *
 from .serializers import *
+from rest_framework import filters
+import django_filters.rest_framework
+from home.serializers import ProductSerializer
+from rest_framework import generics
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend,
+                       filters.SearchFilter,
+                       filters.OrderingFilter,]
+    search_fields =['title','description']
+    ordering_fields = ['id' , 'name' , 'price']
+    filterset_fields = ['category', 'stock', 'brand','labels']
+
+
+class ProductDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ProductSerializer(snippet, data=request.data , partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProductList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        snippets = Product.objects.all()
+        serializer = ProductSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
